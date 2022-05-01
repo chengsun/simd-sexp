ce6a8af6a1e56779263d0f28a22a712080ac3562
Cheng Sun <_@chengsun.uk>
2022-05-01 17:00:22 +0800

    _

---
+++ benches/benches.rs
@@ -0,0 +1,234 @@
+use std::collections::HashSet;
+use criterion::*;
+use simd_sexp::*;
+
+fn bench_lib(c: &mut Criterion) {
+    let input_all_parens_64 = [b'('; 64];
+    let input_all_misc_64 = [b'x'; 64];
+    let input_all_parens_6400 = [b'('; 6400];
+    let input_all_misc_6400 = [b'x'; 6400];
+
+    let mut output_scratch = [0usize; 6400];
+
+    let mut group = c.benchmark_group("lib-64");
+    group.throughput(Throughput::Bytes(64));
+    group.bench_function("all-parens",
+                         |b| b.iter(|| black_box(extract_structural_indices(&input_all_parens_64[..], &mut output_scratch, 0))));
+    group.bench_function("all-misc",
+                         |b| b.iter(|| black_box(extract_structural_indices(&input_all_misc_64[..], &mut output_scratch, 0))));
+    group.finish();
+
+    let mut group = c.benchmark_group("lib-6400");
+    group.throughput(Throughput::Bytes(6400));
+    group.bench_function("all-parens-6400",
+                         |b| b.iter(|| black_box(extract_structural_indices(&input_all_parens_6400[..], &mut output_scratch, 0))));
+    group.bench_function("all-misc-6400",
+                         |b| b.iter(|| black_box(extract_structural_indices(&input_all_misc_6400[..], &mut output_scratch, 0))));
+    group.finish();
+}
+
+fn bench_extract(c: &mut Criterion) {
+    let mut output_scratch = [0usize; 1024];
+
+    let mut group = c.benchmark_group("extract-64");
+    group.throughput(Throughput::Bytes(64));
+    group.bench_function("fast-ones",
+                         |b| b.iter(|| black_box(extract::fast(&mut output_scratch, 0, !0u64))));
+    group.bench_function("fast-zeros",
+                         |b| b.iter(|| black_box(extract::fast(&mut output_scratch, 0, 0u64))));
+    group.bench_function("safe-ones",
+                         |b| b.iter(|| black_box(extract::safe(&mut output_scratch, 0, !0u64))));
+    group.bench_function("safe-zeros",
+                         |b| b.iter(|| black_box(extract::safe(&mut output_scratch, 0, 0u64))));
+    group.finish();
+
+    let mut group = c.benchmark_group("extract-1024");
+    group.throughput(Throughput::Bytes(1024));
+    group.bench_function("fast-ones",
+                         |b| b.iter(|| {
+                             let mut offset = 0;
+                             let mut output_i = 0;
+                             for _ in 0..16 {
+                                 output_i += extract::fast(&mut output_scratch[output_i..], offset, !0u64);
+                                 offset += 64;
+                             }
+                             let _ = black_box(output_i);
+                         } ));
+    group.bench_function("fast-zeros",
+                         |b| b.iter(|| {
+                             let mut offset = 0;
+                             let mut output_i = 0;
+                             for _ in 0..16 {
+                                 output_i += extract::fast(&mut output_scratch[output_i..], offset, 0u64);
+                                 offset += 64;
+                             }
+                             let _ = black_box(output_i);
+                         } ));
+    group.bench_function("safe-ones",
+                         |b| b.iter(|| {
+                             let mut offset = 0;
+                             let mut output_i = 0;
+                             for _ in 0..16 {
+                                 output_i += extract::safe(&mut output_scratch[output_i..], offset, !0u64);
+                                 offset += 64;
+                             }
+                             let _ = black_box(output_i);
+                         } ));
+    group.bench_function("safe-zeros",
+                         |b| b.iter(|| {
+                             let mut offset = 0;
+                             let mut output_i = 0;
+                             for _ in 0..16 {
+                                 output_i += extract::safe(&mut output_scratch[output_i..], offset, 0u64);
+                                 offset += 64;
+                             }
+                             let _ = black_box(output_i);
+                         } ));
+    group.finish();
+}
+
+fn bench_find_quote_transitions(c: &mut Criterion) {
+    let a = rand::random::<u64>();
+    let b = rand::random::<u64>();
+    let unescaped = a & (a ^ b);
+    let escaped = b & (a ^ b);
+    let prev_state = rand::random();
+
+    let mut group = c.benchmark_group("find_quote_transitions");
+    group.throughput(Throughput::Bytes(8));
+    let generic_clmul = clmul::Generic::new();
+    let generic_xor_masked_adjacent = xor_masked_adjacent::Generic::new();
+    group.bench_function("generic",
+                         |b| b.iter(|| black_box(find_quote_transitions::find_quote_transitions(
+                             &generic_clmul, &generic_xor_masked_adjacent,
+                             unescaped, escaped, prev_state))));
+    match (clmul::Sse2Pclmulqdq::new(), xor_masked_adjacent::Bmi2::new()) {
+        (Some(clmul), Some(xor_masked_adjacent)) => {
+            group.bench_function("haswell",
+                                 |b| b.iter(|| black_box(find_quote_transitions::find_quote_transitions(
+                                     &clmul, &xor_masked_adjacent,
+                                     unescaped, escaped, prev_state))));
+        },
+        _ => (),
+    }
+    let runtime_detect_clmul = clmul::runtime_detect();
+    let runtime_detect_xor_masked_adjacent = xor_masked_adjacent::runtime_detect();
+    group.bench_function("runtime-detect",
+                         |b| b.iter(|| black_box(find_quote_transitions::find_quote_transitions(
+                             &runtime_detect_clmul, &runtime_detect_xor_masked_adjacent,
+                             unescaped, escaped, prev_state))));
+    group.finish();
+}
+
+fn bench_start_stop_transitions(c: &mut Criterion) {
+    use start_stop_transitions::StartStopTransitions;
+
+    let a = rand::random::<u64>();
+    let b = rand::random::<u64>();
+    let start = a & (a ^ b);
+    let stop = b & (a ^ b);
+    let prev_state = rand::random();
+
+    let mut group = c.benchmark_group("start_stop_transitions");
+    group.throughput(Throughput::Bytes(8));
+
+    let runtime_detect_start_stop_transitions = start_stop_transitions::runtime_detect();
+    group.bench_function("runtime-detect",
+                         |b| b.iter(|| black_box(runtime_detect_start_stop_transitions.start_stop_transitions(start, stop, prev_state))));
+    let generic_start_stop_transitions = start_stop_transitions::Generic::new(clmul::Generic::new(), xor_masked_adjacent::Generic::new());
+    group.bench_function("generic",
+                         |b| b.iter(|| black_box(generic_start_stop_transitions.start_stop_transitions(start, stop, prev_state))));
+    match start_stop_transitions::Bmi2::new() {
+        None => (),
+        Some(bmi2_start_stop_transitions) => {
+            group.bench_function("bmi2",
+                                 |b| b.iter(|| black_box(bmi2_start_stop_transitions.start_stop_transitions(start, stop, prev_state))));
+        }
+    }
+    group.finish();
+}
+
+fn bench_vector_classifier(c: &mut Criterion) {
+    use simd_sexp::vector_classifier::{Classifier, ClassifierBuilder};
+    let accepting_chars = b" \t\r\n()\\\"";
+    let lookup_tables = vector_classifier::LookupTables::from_accepting_chars(accepting_chars).unwrap();
+
+    let mut naive_classifier: HashSet<u8> = HashSet::new();
+    for &c in accepting_chars {
+        naive_classifier.insert(c);
+    }
+
+    const SIZE: usize = 1000;
+    let bytes = &[0; SIZE];
+
+    let mut group = c.benchmark_group("vector_classifier");
+    group.throughput(Throughput::Bytes(SIZE.try_into().unwrap()));
+    group.bench_function("one-by-one-hash-set",
+                         |b| b.iter(|| {
+                             for byte in bytes {
+                                 let _ = black_box(naive_classifier.contains(byte));
+                             }
+                         }));
+    group.bench_function("one-by-one",
+                         |b| b.iter(|| {
+                             for byte in bytes {
+                                 let _ = black_box(accepting_chars.contains(byte));
+                             }
+                         }));
+    let generic_classifier = vector_classifier::GenericBuilder::new().build(&lookup_tables);
+    group.bench_function("generic",
+                         |b| b.iter_batched(|| bytes.clone(), |mut bytes| {
+                             black_box(generic_classifier.classify(&mut bytes));
+                         }, BatchSize::SmallInput));
+    match vector_classifier::Ssse3Builder::new() {
+        None => (),
+        Some(ssse3_builder) => {
+            let ssse3_classifier = ssse3_builder.build(&lookup_tables);
+            group.bench_function("vector",
+                                 |b| b.iter_batched(|| bytes.clone(), |mut bytes| {
+                                     black_box(ssse3_classifier.classify(&mut bytes));
+                                 }, BatchSize::SmallInput));
+        }
+    }
+    group.finish();
+}
+
+fn bench_xor_masked_adjacent(c: &mut Criterion) {
+    use xor_masked_adjacent::XorMaskedAdjacent;
+
+    let bitstring = rand::random::<u64>();
+    let mut mask;
+    loop {
+        mask = rand::random::<u64>();
+        if mask.count_ones() == 32 { break; }
+    }
+    let lo_fill = rand::random();
+
+    let mut group = c.benchmark_group("xor_masked_adjacent");
+    group.throughput(Throughput::Bytes(8));
+
+    let generic = xor_masked_adjacent::Generic::new();
+    group.bench_function("generic",
+                         |b| b.iter(|| black_box(generic.xor_masked_adjacent(bitstring, mask, lo_fill))));
+    let runtime_detect = xor_masked_adjacent::runtime_detect();
+    group.bench_function("runtime-detect",
+                         |b| b.iter(|| black_box(runtime_detect.xor_masked_adjacent(bitstring, mask, lo_fill))));
+    match xor_masked_adjacent::Bmi2::new() {
+        None => (),
+        Some(bmi2) => {
+            group.bench_function("bmi2",
+                                 |b| b.iter(|| black_box(bmi2.xor_masked_adjacent(bitstring, mask, lo_fill))));
+        }
+    }
+    group.finish();
+}
+
+criterion_group!(benches,
+                 bench_lib,
+                 bench_extract,
+                 bench_find_quote_transitions,
+                 bench_start_stop_transitions,
+                 bench_vector_classifier,
+                 bench_xor_masked_adjacent);
+
+criterion_main!(benches);
---
+++ src/clmul.rs
@@ -0,0 +1,136 @@
+#[cfg(target_arch = "x86")]
+use core::arch::x86::*;
+#[cfg(target_arch = "x86_64")]
+use core::arch::x86_64::*;
+
+pub trait Clmul {
+    fn clmul(&self, input: u64) -> u64;
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Generic {}
+
+impl Generic {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Clmul for Generic {
+    fn clmul(&self, input: u64) -> u64 {
+        let mut output = 0u64;
+        let mut cur_state = 0u64;
+        for bit_index in 0..64 {
+            if (input & (1 << bit_index)) != 0 {
+                cur_state = cur_state ^ 1;
+            }
+            output = output | (cur_state << bit_index)
+        }
+        output
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Sse2Pclmulqdq { _feature_detected_witness: () }
+
+impl Sse2Pclmulqdq {
+    pub fn new() -> Option<Self> {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        if is_x86_feature_detected!("sse2") && is_x86_feature_detected!("pclmulqdq") {
+            return Some(Self { _feature_detected_witness: () });
+        }
+        None
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    #[target_feature(enable = "sse2,pclmulqdq")]
+    unsafe fn _clmul(&self, input: u64) -> u64 {
+        _mm_cvtsi128_si64(_mm_clmulepi64_si128(_mm_set_epi64x(0i64, input as i64), _mm_set1_epi8(0xFFu8 as i8), 0x00)) as u64
+    }
+}
+
+impl Clmul for Sse2Pclmulqdq {
+    fn clmul(&self, input: u64) -> u64 {
+        let () = self._feature_detected_witness;
+        return unsafe { self._clmul(input) };
+    }
+}
+
+impl Clmul for Box<dyn Clmul> {
+    fn clmul(&self, input: u64) -> u64 {
+        (**self).clmul(input)
+    }
+}
+
+pub fn runtime_detect() -> Box<dyn Clmul> {
+    match Sse2Pclmulqdq::new () {
+        None => (),
+        Some(clmul) => { return Box::new(clmul); }
+    }
+    Box::new(Generic::new())
+}
+
+#[cfg(test)]
+mod clmul_tests {
+    use super::*;
+    use crate::utils::*;
+
+    trait Testable {
+        fn run_test(&self, input: u64, output: u64);
+    }
+
+    impl<T: Clmul> Testable for T {
+        fn run_test(&self, input: u64, output: u64) {
+            let input = bitrev64(input);
+            let output = bitrev64(output);
+            let actual_output = self.clmul(input);
+            if output != actual_output {
+                print!("input:      ");
+                print_bitmask_le(input, 64);
+                print!("expect out: ");
+                print_bitmask_le(output, 64);
+                print!("actual out: ");
+                print_bitmask_le(actual_output, 64);
+                panic!("clmul test failed");
+            }
+        }
+    }
+
+    fn run_test(input: u64, output: u64) {
+        let generic = Generic::new();
+        generic.run_test(input, output);
+
+        match Sse2Pclmulqdq::new() {
+            Some(sse2_pclmulqdq) => sse2_pclmulqdq.run_test(input, output),
+            None => (),
+        }
+    }
+
+    #[test]
+    fn test_1() {
+        let input_ = 0b0;
+        let output = 0b0;
+        run_test(input_, output);
+    }
+
+    #[test]
+    fn test_2() {
+        let input_ = 0b1111111111111111111111111111111111111111111111111111111111111111;
+        let output = 0b1010101010101010101010101010101010101010101010101010101010101010;
+        run_test(input_, output);
+    }
+
+    #[test]
+    fn test_3() {
+        let input_ = 0b0111111111111111111111111111111111111111111111111111111111111111;
+        let output = 0b0101010101010101010101010101010101010101010101010101010101010101;
+        run_test(input_, output);
+    }
+
+    #[test]
+    fn test_4() {
+        let input_ = 0b11010010;
+        let output = 0b10011100;
+        run_test(input_, output);
+    }
+}
