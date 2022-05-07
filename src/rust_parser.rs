dc2adb579bfc3ec17aabb7cb63fb0fb9c31ed58e
Cheng Sun <_@chengsun.uk>
2022-05-07 17:58:08 +0100

    working tests for sexp, tape, two_phase::tape

--- src/rust_parser.rs
+++ src/rust_parser.rs
@@ -1,31 +1,35 @@
 use crate::{escape, parser, utils};
 
-pub enum RustSexp {
-    Atom(Vec<u8>),
-    List(Vec<RustSexp>),
+fn write_atom(f: &mut std::fmt::Formatter<'_>, a: &[u8], space_separator_needed: &mut bool) -> std::fmt::Result {
+    if escape::escape_is_necessary(a) {
+        write!(f, "\"")?;
+        write!(f, "{}", String::from_utf8(escape::escape(a)).unwrap())?;
+        write!(f, "\"")?;
+        *space_separator_needed = false;
+    } else {
+        if *space_separator_needed {
+            write!(f, " ")?;
+        }
+        write!(f, "{}", std::str::from_utf8(a).unwrap())?;
+        *space_separator_needed = true;
+    }
+    Ok(())
 }
 
-impl RustSexp {
-    fn fmt_internal(&self, f: &mut std::fmt::Formatter<'_>, space_separator_needed: &mut bool) -> std::fmt::Result {
+pub enum Sexp {
+    Atom(Vec<u8>),
+    List(Vec<Sexp>),
+}
+
+impl Sexp {
+    fn fmt_mach(&self, f: &mut std::fmt::Formatter<'_>, space_separator_needed: &mut bool) -> std::fmt::Result {
         match self {
-            RustSexp::Atom(a) =>
-                if escape::escape_is_necessary(a) {
-                    write!(f, "\"")?;
-                    write!(f, "{}", String::from_utf8(escape::escape(a)).unwrap())?;
-                    write!(f, "\"")?;
-                    *space_separator_needed = false;
-                } else {
-                    if *space_separator_needed {
-                        write!(f, " ")?;
-                    }
-                    write!(f, "{}", std::str::from_utf8(a).unwrap())?;
-                    *space_separator_needed = true;
-                },
-            RustSexp::List(l) => {
+            Sexp::Atom(a) => write_atom(f, a, space_separator_needed)?,
+            Sexp::List(l) => {
                 write!(f, "(")?;
                 *space_separator_needed = false;
                 for s in l.iter() {
-                    s.fmt_internal(f, space_separator_needed)?;
+                    s.fmt_mach(f, space_separator_needed)?;
                 }
                 write!(f, ")")?;
                 *space_separator_needed = false;
@@ -35,19 +39,19 @@ impl RustSexp {
     }
 }
 
-impl std::fmt::Display for RustSexp {
+struct SexpMulti(Vec<Sexp>);
+
+impl std::fmt::Display for Sexp {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.fmt_internal(f, &mut false)
+        self.fmt_mach(f, &mut false)
     }
 }
 
-struct RustSexps(Vec<RustSexp>);
-
-impl std::fmt::Display for RustSexps {
+impl std::fmt::Display for SexpMulti {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut space_separator_needed = false;
         for s in self.0.iter() {
-            s.fmt_internal(f, &mut space_separator_needed)?;
+            s.fmt_mach(f, &mut space_separator_needed)?;
         }
         Ok(())
     }
@@ -62,14 +66,14 @@ impl RustSexpFactory {
 }
 
 impl parser::SexpFactory for RustSexpFactory {
-    type Sexp = RustSexp;
+    type Sexp = Sexp;
 
     fn atom(&self, a: &[u8]) -> Self::Sexp {
-        RustSexp::Atom(a.to_vec())
+        Sexp::Atom(a.to_vec())
     }
 
     fn list(&self, xs: Vec<Self::Sexp>) -> Self::Sexp {
-        RustSexp::List(xs)
+        Sexp::List(xs)
     }
 }
 
@@ -78,14 +82,55 @@ Atom: <len*2 as u32>string
 List: <len*2+1 as u32>Repr(X1)Repr(X2)...
 */
 
+#[derive(Default)]
+pub struct Tape(Vec<u8>);
+
+impl Tape {
+    fn fmt_mach(&self, f: &mut std::fmt::Formatter<'_>, space_separator_needed: &mut bool) -> std::fmt::Result {
+        println!("tape: {:?}", self.0);
+        let mut i = 0usize;
+        let mut list_ends: Vec<usize> = Vec::new();
+        loop {
+            if list_ends.last() == Some(&i) {
+                list_ends.pop();
+                write!(f, ")")?;
+                *space_separator_needed = false;
+            }
+            if i >= self.0.len() {
+                break;
+            }
+            let x = utils::read_u32(&self.0[i..]);
+            i += 4;
+            let is_atom = x % 2 == 0;
+            let len = (x / 2) as usize;
+            if is_atom {
+                write_atom(f, &self.0[i..(i + len)], space_separator_needed)?;
+                i += len;
+            } else {
+                write!(f, "(")?;
+                *space_separator_needed = false;
+                list_ends.push(i + len);
+            }
+        }
+        assert!(list_ends.is_empty());
+        Ok(())
+    }
+}
+
+impl std::fmt::Display for Tape {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.fmt_mach(f, &mut false)
+    }
+}
+
 pub struct TapeVisitor {
-    tape: Vec<u8>,
+    tape: Tape,
 }
 
 impl TapeVisitor {
     pub fn new() -> TapeVisitor {
         Self {
-            tape: Vec::new(),
+            tape: Tape(Vec::new()),
         }
     }
 }
@@ -97,22 +142,23 @@ pub struct TapeVisitorContext {
 impl parser::Visitor for TapeVisitor {
     type Context = TapeVisitorContext;
     type IntermediateReturnType = ();
-    type FinalReturnType = Vec<u8>;
+    type FinalReturnType = Tape;
     fn atom(&mut self, atom: &[u8], _: Option<&mut TapeVisitorContext>) {
-        let tape_start_index = self.tape.len();
-        utils::write_u32(&mut self.tape[tape_start_index..], (atom.len() * 2).try_into().unwrap());
-        self.tape.extend_from_slice(atom);
+        let tape_start_index = self.tape.0.len();
+        self.tape.0.extend([0u8; 4]);
+        utils::write_u32(&mut self.tape.0[tape_start_index..], (atom.len() * 2).try_into().unwrap());
+        self.tape.0.extend_from_slice(atom);
     }
     fn list_open(&mut self, _: Option<&mut TapeVisitorContext>) -> TapeVisitorContext {
-        let tape_start_index = self.tape.len();
-        self.tape.extend([0u8; 4]);
+        let tape_start_index = self.tape.0.len();
+        self.tape.0.extend([0u8; 4]);
         TapeVisitorContext {
             tape_start_index,
         }
     }
     fn list_close(&mut self, context: TapeVisitorContext, _: Option<&mut TapeVisitorContext>) {
-        let x: u32 = ((self.tape.len() - context.tape_start_index) * 2 + 1).try_into().unwrap();
-        utils::write_u32(&mut self.tape[context.tape_start_index..], x);
+        let x: u32 = ((self.tape.0.len() - context.tape_start_index - 4) * 2 + 1).try_into().unwrap();
+        utils::write_u32(&mut self.tape.0[context.tape_start_index..], x);
     }
     fn eof(&mut self) -> Self::FinalReturnType {
         std::mem::take(&mut self.tape)
@@ -127,13 +173,55 @@ mod two_phase {
     List: <len*2+1>Repr(X1)Repr(X2)...
     */
 
-    pub struct Phase1 {
+    #[derive(Default)]
+    pub struct Tape(Vec<u8>);
+
+    impl Tape {
+        fn fmt_mach(&self, f: &mut std::fmt::Formatter<'_>, varint_decoder: &varint::GenericDecoder, space_separator_needed: &mut bool) -> std::fmt::Result {
+            println!("vartape: {:?}", self.0);
+            let mut i = 0usize;
+            let mut list_ends: Vec<usize> = Vec::new();
+            loop {
+                if list_ends.last() == Some(&i) {
+                    list_ends.pop();
+                    write!(f, ")")?;
+                    *space_separator_needed = false;
+                }
+                if i >= self.0.len() {
+                    break;
+                }
+                let mut x = 0usize;
+                i += varint_decoder.decode_one(&self.0[i..], &mut x).unwrap();
+                let is_atom = x % 2 == 0;
+                let len = (x / 2) as usize;
+                if is_atom {
+                    super::write_atom(f, &self.0[i..(i + len)], space_separator_needed)?;
+                    i += len;
+                } else {
+                    write!(f, "(")?;
+                    *space_separator_needed = false;
+                    list_ends.push(i + len);
+                }
+            }
+            assert!(list_ends.is_empty());
+            Ok(())
+        }
+    }
+
+    impl std::fmt::Display for Tape {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let varint_decoder = varint::GenericDecoder::new();
+            self.fmt_mach(f, &varint_decoder, &mut false)
+        }
+    }
+
+    pub struct Phase1Visitor {
         varint_encoder: varint::GenericEncoder,
         varint_length_tape: Vec<u8>,
         size: usize,
     }
 
-    impl Phase1 {
+    impl Phase1Visitor {
         pub fn new() -> Self {
             Self {
                 varint_encoder: varint::GenericEncoder::new(),
@@ -148,7 +236,7 @@ mod two_phase {
         varint_length_tape_index: usize,
     }
 
-    impl parser::Visitor for Phase1 {
+    impl parser::Visitor for Phase1Visitor {
         type Context = Phase1Context;
         type IntermediateReturnType = ();
         type FinalReturnType = (usize, Vec<u8>);
@@ -183,7 +271,7 @@ mod two_phase {
     }
 
     pub struct Phase2Visitor {
-        tape: Vec<u8>,
+        tape: Tape,
         tape_index: usize,
         varint_length_tape: Vec<u8>,
         varint_length_tape_index: usize,
@@ -191,10 +279,10 @@ mod two_phase {
     }
 
     impl Phase2Visitor {
-        pub fn new(phase1_result: <Phase1 as parser::Visitor>::FinalReturnType) -> Phase2Visitor {
+        pub fn new(phase1_result: <Phase1Visitor as parser::Visitor>::FinalReturnType) -> Phase2Visitor {
             let (size, varint_length_tape) = phase1_result;
             Self {
-                tape: vec![0u8; size],
+                tape: Tape(vec![0u8; size]),
                 tape_index: 0usize,
                 varint_length_tape,
                 varint_length_tape_index: 0usize,
@@ -205,17 +293,18 @@ mod two_phase {
 
     pub struct Phase2Context {
         tape_start_index: usize,
+        varint_length: u8,
     }
 
     impl parser::Visitor for Phase2Visitor {
         type Context = Phase2Context;
         type IntermediateReturnType = ();
-        type FinalReturnType = Vec<u8>;
+        type FinalReturnType = Tape;
         fn atom(&mut self, atom: &[u8], _: Option<&mut Phase2Context>) {
-            let varint_len = self.varint_encoder.encode_one(atom.len() * 2, &mut self.tape[self.tape_index..]).unwrap();
+            let varint_len = self.varint_encoder.encode_one(atom.len() * 2, &mut self.tape.0[self.tape_index..]).unwrap();
             self.tape_index += varint_len;
             for &a in atom {
-                self.tape[self.tape_index] = a;
+                self.tape.0[self.tape_index] = a;
                 self.tape_index += 1;
             }
         }
@@ -226,10 +315,14 @@ mod two_phase {
             self.tape_index += varint_length as usize;
             Phase2Context {
                 tape_start_index,
+                varint_length,
             }
         }
         fn list_close(&mut self, context: Phase2Context, _: Option<&mut Phase2Context>) {
-            self.varint_encoder.encode_one((self.tape_index - context.tape_start_index) * 2 + 1, &mut self.tape[context.tape_start_index..]);
+            let varint_length = context.varint_length as usize;
+            self.varint_encoder.encode_one(
+                (self.tape_index - context.tape_start_index - varint_length) * 2 + 1,
+                &mut self.tape.0[context.tape_start_index..(context.tape_start_index + varint_length)]);
         }
         fn eof(&mut self) -> Self::FinalReturnType {
             std::mem::take(&mut self.tape)
@@ -242,19 +335,46 @@ mod parser_tests {
     use super::*;
 
     fn run_test(input: &[u8], expected_output: Result<&str, parser::Error>) {
-        let mut parser = parser::State::new(parser::SimpleVisitor::new(RustSexpFactory::new()));
-        let sexp_or_error = parser.process_all(input);
-        let output = sexp_or_error.map(|sexp| RustSexps(sexp).to_string());
-        let output = match output {
-            Ok(ref x) => Ok(x.as_str()),
-            Err(e) => Err(e),
+        let validate = |name: &str, output: Result<String, parser::Error>| {
+            let output = match output {
+                Ok(ref x) => Ok(x.as_str()),
+                Err(e) => Err(e),
+            };
+
+            if output != expected_output {
+                    println!("input:      {:?}", input);
+                    println!("expect out: {:?}", expected_output);
+                    println!("actual out: {:?}", output);
+                    panic!("parser test failed for {}", name);
+            }
         };
 
-        if output != expected_output {
-                println!("input:      {:?}", input);
-                println!("expect out: {:?}", expected_output);
-                println!("actual out: {:?}", output);
-                panic!("parser test failed");
+        {
+            let mut parser = parser::State::new(parser::SimpleVisitor::new(RustSexpFactory::new()));
+            let sexp_or_error = parser.process_all(input);
+            let output = sexp_or_error.map(|sexps| SexpMulti(sexps).to_string());
+            validate("SimpleVisitor<RustSexpFactory>", output);
+        }
+
+        {
+            let mut parser = parser::State::new(TapeVisitor::new());
+            let sexp_or_error = parser.process_all(input);
+            let output = sexp_or_error.map(|tape| tape.to_string());
+            validate("TapeVisitor", output);
+        }
+
+        {
+            let mut phase1 = parser::State::new(two_phase::Phase1Visitor::new());
+            let phase1_result = phase1.process_all(input);
+            let tape_or_error = match phase1_result {
+                Err(e) => Err(e),
+                Ok(phase1_result) => {
+                    let mut phase2 = parser::State::new(two_phase::Phase2Visitor::new(phase1_result));
+                    phase2.process_all(input)
+                },
+            };
+            let output = tape_or_error.map(|tape| tape.to_string());
+            validate("two_phase", output);
         }
 
     }
