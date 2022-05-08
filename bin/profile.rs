c87623acd1e109860b92497e83f5cd416d10004c
Cheng Sun <_@chengsun.uk>
2022-05-08 07:04:05 +0100

    _

--- bin/profile.rs
+++ bin/profile.rs
@@ -43,10 +43,10 @@ fn main() {
         println!("# lists:        {}", sexp_result.iter().map(count_lists).sum::<usize>());
     }
 
-    let event_frame = ittapi::Event::new("frame");
-
     match std::env::args().nth(1).as_deref() {
         Some("tape") => {
+            let event_frame = ittapi::Event::new("frame");
+
             println!("Warmup");
 
             for _i in 0..10000 {
@@ -69,6 +69,9 @@ fn main() {
         },
 
         Some("two_phase") => {
+            let event_phase1 = ittapi::Event::new("phase1");
+            let event_phase2 = ittapi::Event::new("phase2");
+
             println!("Warmup");
 
             for _i in 0..10000 {
@@ -84,15 +87,23 @@ fn main() {
             println!("Profiling");
 
             for _i in 0..10000 {
-                let e = event_frame.start();
-                let mut input_pp_v = input_pp.to_vec();
-                let mut parser = parser::State::new(rust_parser::two_phase::Phase1Visitor::new());
-                let result = parser.process_all(&mut input_pp_v[..]).unwrap();
-                input_pp_v.copy_from_slice(input_pp);
-                let mut parser = parser::State::new(rust_parser::two_phase::Phase2Visitor::new(result));
-                let result = parser.process_all(&mut input_pp_v[..]).unwrap();
+                let (mut input_pp_v, result) = {
+                    let e = event_phase1.start();
+                    let mut input_pp_v = input_pp.to_vec();
+                    let mut parser = parser::State::new(rust_parser::two_phase::Phase1Visitor::new());
+                    let result = parser.process_all(&mut input_pp_v[..]).unwrap();
+                    std::mem::drop(e);
+                    (input_pp_v, result)
+                };
+                let result = {
+                    let e = event_phase2.start();
+                    input_pp_v.copy_from_slice(input_pp);
+                    let mut parser = parser::State::new(rust_parser::two_phase::Phase2Visitor::new(result));
+                    let result = parser.process_all(&mut input_pp_v[..]).unwrap();
+                    std::mem::drop(e);
+                    result
+                };
                 criterion::black_box(result);
-                std::mem::drop(e);
             }
         },
 
