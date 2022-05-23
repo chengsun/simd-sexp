c6a6bcfae703c77322c9fa34c408c0d1aeefc58d
Cheng Sun <_@chengsun.uk>
2022-05-23 08:06:11 +0100

    add select to profile binary

--- bin/profile.rs
+++ bin/profile.rs
@@ -1,4 +1,5 @@
 use simd_sexp::*;
+use std::io::BufReader;
 
 fn main() {
     let input_pp = std::fs::read_to_string("/home/user/simd-sexp/test_data.mach.sexp").unwrap();
@@ -6,7 +7,7 @@ fn main() {
 
     {
         let mut sexp_parser = parser::State::from_sexp_factory(rust_parser::SexpFactory::new());
-        let sexp_result = sexp_parser.process_all(&input_pp[..]).unwrap();
+        let sexp_result = sexp_parser.process_all(parser::SegmentIndex::EntireFile, &input_pp[..]).unwrap();
         fn count_atoms(sexp: &rust_parser::Sexp) -> usize {
             match sexp {
                 rust_parser::Sexp::Atom(_) => 1,
@@ -26,7 +27,7 @@ fn main() {
             }
         }
         let mut tape_parser = parser::State::from_visitor(rust_parser::TapeVisitor::new());
-        let tape_result = tape_parser.process_all(&input_pp[..]).unwrap();
+        let tape_result = tape_parser.process_all(parser::SegmentIndex::EntireFile, &input_pp[..]).unwrap();
         println!("Input size:     {}", input_pp.len());
         println!("Of which atoms: {}", sexp_result.iter().map(count_atom_size).sum::<usize>());
         println!("Tape size:      {}", std::mem::size_of_val(&*tape_result.tape) + std::mem::size_of_val(&*tape_result.atoms));
@@ -42,7 +43,7 @@ fn main() {
 
             for _i in 0..10000 {
                 let mut parser = parser::State::from_visitor(rust_parser::TapeVisitor::new());
-                let result = parser.process_all(&input_pp[..]);
+                let result = parser.process_all(parser::SegmentIndex::EntireFile, &input_pp[..]);
                 criterion::black_box(result.unwrap());
             }
 
@@ -51,14 +52,38 @@ fn main() {
             for _i in 0..10000 {
                 let e = event_frame.start();
                 let mut parser = parser::State::from_visitor(rust_parser::TapeVisitor::new());
-                let result = parser.process_all(&input_pp[..]);
+                let result = parser.process_all(parser::SegmentIndex::EntireFile, &input_pp[..]);
                 criterion::black_box(result.unwrap());
                 std::mem::drop(e);
             }
         },
 
+        Some("select") => {
+            let keys = [&b"name"[..], &b"libraries"[..]];
+
+            let event_frame = ittapi::Event::new("frame");
+
+            println!("Warmup");
+
+            for _i in 0..10000 {
+                let mut result = Vec::new();
+                let () = select_parallel::process_streaming(keys, &mut BufReader::new(input_pp), &mut result).unwrap();
+                criterion::black_box(result);
+            }
+
+            println!("Profiling");
+
+            for _i in 0..10000 {
+                let e = event_frame.start();
+                let mut result = Vec::new();
+                let () = select_parallel::process_streaming(keys, &mut BufReader::new(input_pp), &mut result).unwrap();
+                criterion::black_box(result);
+                std::mem::drop(e);
+            }
+        },
+
         arg => {
-            println!("Not profiling. In order to profile, pass \"tape\" as an argument.");
+            println!("Not profiling. In order to profile, pass \"tape\" or \"select\" as an argument.");
             println!("You passed: {:?}", arg);
         },
     }
