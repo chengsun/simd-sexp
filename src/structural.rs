chengsun/simd-sexp

commit bed39e8d9e9f933b408d13366f09e5c00ee24ebf

neon: fix silly pessimisation

src/structural.rs
@@ -342,10 +342,8 @@ mod aarch64 {
 
             let parens = vorrq_u8(lparen, rparen);
 
-            let mut a = [0u8; 16];
-            vst1q_u8(&mut a as *mut u8, input);
-            self.atom_terminator_classifier.classify(&mut a[..]);
-            let atom_like = vld1q_u8(&mut a as *mut u8);
+            let atom_like = input.clone();
+            self.atom_terminator_classifier.classify_neon(&mut [atom_like]);
             let atom_like = vceqq_u8(atom_like, vdupq_n_u8(0));
 
             ClassifyOneNeon {

src/vector_classifier.rs
@@ -283,7 +283,7 @@ mod neon {
 
         #[target_feature(enable = "neon")]
         #[inline]
-        pub unsafe fn classify_avx2(&self, in_out: &mut [uint8x16_t]) {
+        pub unsafe fn classify_neon(&self, in_out: &mut [uint8x16_t]) {
             let lo_nibble_epi8 = vdupq_n_u8(0xF);
 
             for i in 0..in_out.len() {
@@ -299,7 +299,7 @@ mod neon {
         fn classify(&self, in_out: &mut [u8]) {
             let (prefix, aligned, suffix) = unsafe { in_out.align_to_mut::<uint8x16_t>() };
             self.generic.classify(prefix);
-            unsafe { self.classify_avx2(aligned); }
+            unsafe { self.classify_neon(aligned); }
             self.generic.classify(suffix);
         }
     }

commit 19a38f0494cb95e25f49ae3f04c73a2338aca26a

fix bug introduced in bed39e8d9e9f933b408d13366f09e5c00ee24ebf

src/structural.rs
@@ -338,8 +338,8 @@ mod aarch64 {
 
             let parens = vorrq_u8(lparen, rparen);
 
-            let atom_like = input.clone();
-            self.atom_terminator_classifier.classify_neon(&mut [atom_like]);
+            let mut atom_like = input.clone();
+            self.atom_terminator_classifier.classify_neon(std::slice::from_mut(&mut atom_like));
             let atom_like = vceqq_u8(atom_like, vdupq_n_u8(0));
 
             ClassifyOneNeon {